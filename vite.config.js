7c4cc3114449d06a38e6d424f29a857cd1a6ee5b
Shourya <shourya124sensei@gmail.com>
2025-07-20 02:49:08 +0530

    Vercel update in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/VocalizeIt/',
+  base: '/', // ✅ Correct for Vercel (served from root)
   plugins: [react()],
 })
